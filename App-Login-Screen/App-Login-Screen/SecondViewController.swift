fad9954afe41c8636024f8b4579d14f0743d6082
Ruslan Galiev <lakers-kobe@yandex.ru>
2023-04-06 15:17:32 -0700

    Lesson code

---
+++ App-Login-Screen/App-Login-Screen/SecondViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SecondViewController.swift
+//  App-Login-Screen
+//
+//  Created by Ruslan Galiev on 4/6/23.
+//
+
+import UIKit
+
+final class SecondViewController: UIViewController {
+    
+}
---
+++ App-Login-Screen/App-Login-Screen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App-Login-Screen
+//
+//  Created by Ruslan Galiev on 4/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
